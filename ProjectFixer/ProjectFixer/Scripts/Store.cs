600df8c6059940cb7bd88d6952da381c5916c98f
Navron Quixley <Navron.Quixley@Transmax.com.au>
2018-05-31 15:15:59 +1000

    Update

---
+++ ProjectFixer/ProjectFixer/Scripts/Store.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+
+namespace ProjectFixer.Scripts
+{
+    [Verb("Store", HelpText = "Debugging Verb, used to create json files of processed visual studio project and make files")]
+    public class StoreTest : Options
+    {
+
+        [Option("MakeFiles", HelpText = "Scans Make Files")]
+        public bool DoMakeFiles { get; set; }
+
+        [Option("CSharp", HelpText = "Scans CSharp Projects")]
+        public bool DoCsFiles { get; set; }
+
+        [Option("Cpp", HelpText = "Scans C++ Projects")]
+        public bool DoCppFiles { get; set; }
+
+        public void Run()
+        {
+            var store = new Data.Store(this);
+
+            if (DoMakeFiles) store.BuildMakeFiles();
+            if (DoCsFiles) store.BuildVisualStudioCSharp();
+            if (DoCppFiles) store.BuildVisualStudioCpp();
+        }
+    }
+}
